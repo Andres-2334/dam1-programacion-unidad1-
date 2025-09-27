4cfef50b99d94e6340cf5cc0b18f2b2fa1cbbea3
Iván <ivan.corrales@wesovilabs.com>
2025-09-27 14:09:32 +0200

    review TarjetaArtista

--- 26-09-2025 actividades/TarjetaArtista.java
+++ 26-09-2025 actividades/TarjetaArtista.java
@@ -2,9 +2,9 @@ public class TarjetaArtista {
 
     public static void main(String[] args) {
         System.out.println("********************");
-        System.out.println("Artista:   Selena");
-        System.out.println("Edad:      30");
-        System.out.println("Género:    Pop");
+        System.out.println("Artista:\tSelena");
+        System.out.println("Edad:\t\t30");
+        System.out.println("Género:\t\tPop");
         System.out.println("********************");
     }
 }
