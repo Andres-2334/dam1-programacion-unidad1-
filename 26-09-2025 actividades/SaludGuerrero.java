c468907558d80a61296ec11d317c6fc5b0bf8bae
Iván <ivan.corrales@wesovilabs.com>
2025-09-27 14:13:29 +0200

    review SaludGuerrero

--- 26-09-2025 actividades/SaludGuerrero.java
+++ 26-09-2025 actividades/SaludGuerrero.java
@@ -1,22 +1,20 @@
 import java.util.Scanner;
 
 public class SaludGuerrero {
-public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-    int saludguerrero = 100;
-    int pocion = 18;
-    int golpe = -12;
-    System.out.println("golpes recibidos: ");
-    int golperecibido = sc.nextInt();
-    System.out.println("pocion bebida: ");
-    int pocionBebida = sc.nextInt();
-    System.out.println("Salud final: " + (saludguerrero + (golperecibido * golpe) + (pocion * pocionBebida)));
+        int saludguerrero = 100;
+        final int PORCION_SALUD = 18;
+        final int GOLPE_SALUD = 12;
+        System.out.println("golpes recibidos: ");
+        int golperecibido = sc.nextInt();
+        System.out.println("pocion bebida: ");
+        int pocionBebida = sc.nextInt();
+        saludguerrero -= golperecibido * GOLPE_SALUD;
+        saludguerrero += pocionBebida * PORCION_SALUD;
+        saludguerrero = Math.min(Math.max(0, saludguerrero), 100);
+        System.out.printf("Salud final: %d%n", saludguerrero);
     
-
     }
-    
-    
-
-
 
 }
