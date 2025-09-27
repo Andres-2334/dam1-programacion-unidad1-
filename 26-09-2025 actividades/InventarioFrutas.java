461330ddcfbdc838941632aabf2ef7711939affb
Iván <ivan.corrales@wesovilabs.com>
2025-09-27 14:15:17 +0200

    review InventarioFrutas

--- 26-09-2025 actividades/InventarioFrutas.java
+++ 26-09-2025 actividades/InventarioFrutas.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
 
 public class InventarioFrutas {
-public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    int frutaliacomienzo = 200;
-    System.out.println("Frutas recibidas: ");
-    int frutasrecibidas = sc.nextInt();
-    System.out.println("Frutas vendidas: ");
-    int frutasvendidas = sc.nextInt();
-    System.out.println("inventario final: " + (frutaliacomienzo + frutasrecibidas - frutasvendidas));
+        int frutasTotales = 200;
+        System.out.println("Frutas recibidas: ");
+        int frutasRecibidas = sc.nextInt();
+        System.out.println("Frutas vendidas: ");
+        int frutasVendidas = sc.nextInt();
+        frutasTotales += frutasRecibidas - frutasVendidas;
 
+        System.out.printf("inventario final: %d%n", frutasTotales);
 
-}
+        sc.close();
+
+    }
 }
